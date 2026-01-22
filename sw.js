5c23b91452abbea8804968a69ffab91f076be2d4
Adrian Czesnowski <adrian.czesnowski22@gmail.com>
2026-01-22 20:57:28 +0100

    working on app

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // KONFIGURACJA SERVICE WORKERA
 // =====================================================
 
-const CACHE_VERSION = "v13";
+const CACHE_VERSION = "v14";
 const APP_SHELL_CACHE = `app-shell-${CACHE_VERSION}`;
 const RUNTIME_CACHE = `runtime-${CACHE_VERSION}`;
 
